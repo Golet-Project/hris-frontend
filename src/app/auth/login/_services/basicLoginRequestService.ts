96808163ea2eded8bbbadde7ad906c6baee9918d
yuu <yogistanada252@gmail.com>
2023-08-12 15:12:41 +0700

    refactor: auth

--- src/app/auth/login/_services/basicLoginRequestService.ts
+++ src/app/auth/login/_services/basicLoginRequestService.ts
@@ -2,7 +2,7 @@
 
 import { APP_ID } from "@/utils/constant"
 import { HttpBaseResponseBodyJson, HttpResponse, proxyUrl } from "@/utils/http"
-import { cookies } from "next/headers"
+import { cookies, headers } from "next/headers"
 
 type BasicLoginIn = {
   email: string
@@ -23,56 +23,64 @@ type SubmitFormOut = HttpResponse
  */
 export default async function basicLoginRequest(params: BasicLoginIn): Promise<SubmitFormOut> {
   const url = proxyUrl("/auth/login")
+  const headerList = headers()
   const body = {
     email: params.email,
     password: params.password
   }
 
-  const response = await fetch(url, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      "X-App-ID": APP_ID
-    },
-    body: JSON.stringify(body),
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "X-App-ID": APP_ID,
+        "User-Agent": headerList.get("user-agent") ?? ""
+      },
+      body: JSON.stringify(body),
 
-    next: {
-      revalidate: 0
-    }
-  })
+      next: {
+        revalidate: 0
+      }
+    })
 
-  if (!response.ok) {
-    const json = (await response.json()) as HttpBaseResponseBodyJson<null>
-    return {
-      error: {
-        message: json.message,
-        code: response.status
+    if (!response.ok) {
+      const json = (await response.json()) as HttpBaseResponseBodyJson<null>
+      return {
+        error: {
+          message: json.message,
+          code: response.status
+        }
       }
     }
-  }
 
-  const json = (await response.json()) as HttpBaseResponseBodyJson<LoginActionApiResponse>
+    const json = (await response.json()) as HttpBaseResponseBodyJson<LoginActionApiResponse>
 
-  const cookiesExpiredIn = new Date()
-  const time = cookiesExpiredIn.getTime()
-  cookiesExpiredIn.setTime(time + 1000 * 604800)
+    const cookiesExpiredIn = new Date()
+    const time = cookiesExpiredIn.getTime()
+    cookiesExpiredIn.setTime(time + 1000 * 604800)
 
-  if (json.data !== undefined) {
-    cookies().set({
-      name: "token",
-      value: json.data.access_token,
-      httpOnly: true,
-      sameSite: "lax",
-      expires: cookiesExpiredIn,
-      path: "/"
-      // TODO: add secure option
-    })
-  }
-
-  return {
-    success: {
-      message: "Berhasil Login",
-      data: undefined
+    if (json.data !== undefined) {
+      cookies().set({
+        name: "token",
+        value: json.data.access_token,
+        httpOnly: true,
+        sameSite: "lax",
+        expires: cookiesExpiredIn,
+        path: "/"
+        // TODO: add secure option
+      })
     }
+
+    return {
+      success: {
+        message: "Berhasil Login",
+        data: undefined
+      }
+    }
+  } catch (error) {
+    // TODO: send to logger
+    console.log(error)
+    throw error
   }
 }
